64bfe1ee36f4418bd455d9d891c66376cec7ad58
Mitchell Adair <mitchwadair@gmail.com>
2026-05-25 00:01:37 -0400

    fix typo in JSDoc

--- mod.ts
+++ mod.ts
@@ -3,9 +3,8 @@
  *
  * MULSID — Mini Unique Lexicographically-Sortable IDs.
  *
- * MULSID produces 10-character base62 IDs using a bit-packed integer
- * a bit-packed integer with an 18-bit randomness value and a timestamp
- * tick derived from 9 ms increments.
+ * MULSID produces 10-character base62 IDs using a bit-packed integer with an
+ * 18-bit randomness value and a timestamp tick derived from 9 ms increments.
  */
 
 export { monotonicMULSID } from "./src/monotonic";
